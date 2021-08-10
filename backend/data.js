45be2176020afd142bb1da78cd269555b7afd65f
Vaibhav Kakkar <vaibhavkakkar3233@gmail.com>
2021-08-10 15:42:57 +0530

    MongoDB

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,21 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Vaibhav',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Rohan',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+
+  ],
     products: [
       {
         _id: '1',
